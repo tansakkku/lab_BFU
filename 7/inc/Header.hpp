fa096a6a8acd1557b267c1dd7af3c11e9fd796eb
Danil <the.clint.fi@gmail.com>
2021-12-17 22:05:33 +0300

    lab_7

--- 7/inc/Header.hpp
+++ 7/inc/Header.hpp
@@ -5,9 +5,13 @@ const int v = 100;
 
 namespace hd
 {
+	void Input(int n, int m, int mas[v][v]);
+
+	void Output(int n, int m, int mas[v][v]);
+
 	bool Min(int mas[v][v], int n, int m);
 
 	bool Max(int mas[v][v], int n, int m);
 
-	void Sum_Strok_i_Sort(int sum[w], int mas[v][v], int n, int m);
+	void Proizved_Strok_i_Sort(int sum[w], int mas[v][v], int n, int m);
 }
